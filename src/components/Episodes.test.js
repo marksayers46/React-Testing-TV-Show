a3d3f2dd3cb27973d77fdae4dbfa0041fd3a7957
Mark Sayers <marksayers46@icloud.com>
2020-08-05 22:03:58 -0500

    working on test eepisodes

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,1 +1,8 @@
-import React from 'react';
+import React from "react";
+import { render } from "@testing-library/react";
+import Episodes from "./Episodes";
+
+test("is episodes rendering", () => {
+  // arrange
+  render(<Episodes episodes={[]} />);
+});
